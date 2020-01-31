4b0eb025b12211b24dcbcd3702d7b1bd58648b3a
Geetanjali Makineni <59195740+geetamakineni@users.noreply.github.com>
2020-01-31 13:08:04 -0600

    Delete Q1.py

--- Q1.py
+++
@@ -1,9 +0,0 @@
-lst_lb = []
-lst_kg = []
-n = int(input("enter the number of students:"))
-print("Enter the Weights of students in lbs:")
-for j in range(n):
-    elements = int(input())
-    lst_lb.append(elements)
-    lst_kg.append(elements*(0.453514))
-print("The weights of students in kgs are:", lst_kg)
